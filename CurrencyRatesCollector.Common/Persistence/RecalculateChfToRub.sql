59fc00d2b09e87e63caedd40613deba5143dcea9
Aleksandr Belousov <aleksandr.belousov@swisschain.io>
2020-09-04 11:10:40 +0400

    Added rounding to sql script

--- CurrencyRatesCollector.Common/Persistence/RecalculateChfToRub.sql
+++ CurrencyRatesCollector.Common/Persistence/RecalculateChfToRub.sql
@@ -3,7 +3,7 @@
 	chf_rate."CreatedAt", 
 	chf_rate."Value" as "CHF_Value", 
 	rub_rate."Value" as "RUB_Value", 
-	(rub_rate."Value" / chf_rate."Value") as Value
+	ROUND(rub_rate."Value" / chf_rate."Value", 4) as Value
 	FROM currency_rates_collector.currency_rates as chf_rate
 	INNER JOIN currency_rates_collector.currency_rates as rub_rate 
 	ON chf_rate."CreatedAt" = rub_rate."CreatedAt" and rub_rate."Currency" = 'RUB'
